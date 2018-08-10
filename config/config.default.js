968404fd39f6512901f37a935e34891444b7655b
wxaxiaoyao <765485868@qq.com>
2018-08-10 11:46:01 +0800

    update config

--- config/config.default.js
+++ config/config.default.js
@@ -1,28 +1,6 @@
 
 exports.keys = "lesson";
 
-exports.self = {
-	apiUrlPrefix: "/api/v0/",
-	keepworkBaseURL: "",
-	trustIps: [
-		"127.0.0.1",
-		"120.132.120.183",
-	    "120.132.120.161",
-	    "121.14.117.252",
-	    "121.14.117.251",
-	]
-}
-
-
-exports.sequelize = {
-	dialect: "mysql",
-	database: "lesson-dev",
-	host: '10.28.18.16',
-	port: "32000",
-	username: "root",
-	password: "root",
-}
-
 exports.cors = {
 	origin: "*",
 }
