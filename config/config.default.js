714cab5c6f045c302a81530a5a98db6fd88b825e
wxaxiaoyao <765485868@qq.com>
2018-08-17 15:15:45 +0800

    update code

--- config/config.default.js
+++ config/config.default.js
@@ -19,7 +19,11 @@ exports.security = {
 exports.onerror = {
 	all: (e, ctx) => {
 		const message = e.stack || e.message || e.toString();
+
 		ctx.status = e.status || 500;
 		ctx.body = message;
+		if (e.name == "SequelizeUniqueConstraintError") {
+			ctx.status = 409;
+		}
 	}
 }
