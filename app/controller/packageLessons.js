ac39791ddf6de3f140a47dabd68f2e94b0171bdb
wxaxiaoyao <765485868@qq.com>
2019-03-01 15:16:22 +0800

    fix bug

--- app/controller/packageLessons.js
+++ app/controller/packageLessons.js
@@ -3,6 +3,9 @@ const _ = require("lodash");
 const Controller = require("../core/baseController.js");
 
 class PackageLessonsController extends Controller {
+	get modelName() {
+		return "packageLessons";
+	}
 }
 
 module.exports = PackageLessonsController;
