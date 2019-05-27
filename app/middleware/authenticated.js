b97e7fdcaba2ae364713efecade09b039e7231aa
wxaxiaoyao <765485868@qq.com>
2019-05-27 18:50:13 +0800

    fix bug

--- app/middleware/authenticated.js
+++ app/middleware/authenticated.js
@@ -1,18 +1,11 @@
 
 const axios = require("axios");
-const memoryCache = require('memory-cache');
 //const jwt = require("jwt-simple");
 const jwt = require("../core/jwt.js");
 
 module.exports = (options, app) => {
 	const config = app.config.self;
 	return async function(ctx, next) {
-		if (config.debug) {
-			ctx.state.user = {userId:137, username:"wxaxiaoyao", roleId:10};
-			await next();
-			return ;
-		}
-
 		const Authorization =  ctx.request.header["authorization"] || ("Bearer " + (ctx.cookies.get("token") || ""));
 		const token = Authorization.split(" ")[1] || "";
 		const headers = {"Authorization": Authorization};
@@ -20,22 +13,13 @@ module.exports = (options, app) => {
 
 		try {
 			user = jwt.decode(token, config.secret);
+			// 验证token是否有效
+			await axios.get(config.coreServiceBaseUrl + "users/profile", {headers}).catch(e => user = undefined);
 		} catch(e) {
-			user = memoryCache.get(Authorization);
 		}
-
-		//if (!user && token.trim()) {
-			//user = await axios.get(config.tokenUrl, {headers})
-				//.then(res => res.data)
-				//.catch(e => {console.log(e); return undefined;});
-			//if (user && user.userId) {
-				//memoryCache.put(Authorization, user, 1000 * 60 * 60);
-			//}
-		//}
 
 		ctx.state.token = token;
 		ctx.state.user = user || {};
-		//console.log(ctx.state.user);
 
 		await next();
 	}
